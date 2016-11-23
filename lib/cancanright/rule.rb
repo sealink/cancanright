6c0421e68bd7f0128fd713cc26ddf89d0063f022
Grant Colegate <blaknite@thelanbox.com.au>
2016-11-23 10:52:07 +1030

    Rule builder for creating dynamic CanCan rules

---
+++ lib/cancanright/rule.rb
@@ -0,0 +1,93 @@
+module CanCanRight
+  class Rule
+    def self.rule_for(right)
+      (right.controller.present? ? ControllerRightRule : DefaultRightRule).new(right).call
+    end
+
+    def initialize(right)
+      @right = right
+    end
+
+    def call
+      fail NotImplementedError, 'must be implemented in sub-class'
+    end
+
+    private
+
+    def can?
+      true
+    end
+
+    def action
+      :access
+    end
+
+    def subject
+      [@right.controller, @right.action].compact.join('#')
+    end
+
+    def conditions
+      nil
+    end
+  end
+
+  class ControllerRule < Rule
+    def initialize(right)
+      @right = right
+    end
+
+    def call
+      if @right.controller.present?
+        CanCan::Rule.new(can?, action, subject, conditions, nil)
+      else
+        fail CanCanRight::Error, 'must specify controller'
+      end
+    end
+
+    private
+
+    def can?
+      true
+    end
+
+    def action
+      :perform
+    end
+
+    def subject
+      @right.controller + '#' + (@right.action || '*')
+    end
+
+    def conditions
+      nil
+    end
+  end
+
+  class DefaultRule < Rule
+    def initialize(right)
+      @right = right
+    end
+
+    def call
+      CanCan::Rule.new(can?, action, subject, conditions, nil)
+    end
+
+    private
+
+    def can?
+      true
+    end
+
+    def action
+      @right.title.parameterize('_').to_sym
+    end
+
+    def subject
+      nil
+    end
+
+    def conditions
+      nil
+    end
+  end
+end
