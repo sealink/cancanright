780de0f06e496a708d68f5006c36c4ce3c965fb4
Grant Colegate <blaknite@thelanbox.com.au>
2016-11-25 14:32:49 +1030

    Ability should be a module

--- lib/cancanright/ability.rb
+++ lib/cancanright/ability.rb
@@ -1,5 +1,5 @@
 module CanCanRight
-  class Ability < CanCan::Ability
+  module Ability < CanCan::Ability
     private def add_rule_for(right)
       add_rule(CanCanRight::Rule.rule_for(right))
     end
