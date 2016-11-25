3737bae40e68ce21a783786be3475ccfab072ded
Grant Colegate <blaknite@thelanbox.com.au>
2016-11-25 14:30:41 +1030

    include all modules and classes

--- lib/cancanright.rb
+++ lib/cancanright.rb
@@ -1,5 +1,7 @@
 require 'cancanright/model/right'
 require 'cancanright/model/role'
+require 'cancanright/ability'
 require 'cancanright/controller_additions'
 require 'cancanright/error'
+require 'cancanright/role_model'
 require 'cancanright/rule'
