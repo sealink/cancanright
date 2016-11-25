ef649c11db7cbe4b7ccf75dfd758b007f5bb560f
Grant Colegate <blaknite@thelanbox.com.au>
2016-11-25 14:34:11 +1030

    module should include the module not inherit

--- lib/cancanright/ability.rb
+++ lib/cancanright/ability.rb
@@ -1,5 +1,7 @@
 module CanCanRight
-  module Ability < CanCan::Ability
+  module Ability
+    include CanCan::Ability
+
     private def add_rule_for(right)
       add_rule(CanCanRight::Rule.rule_for(right))
     end
