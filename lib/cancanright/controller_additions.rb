61cda4526f178b69eb8d4172fbe98fc404f051b3
Grant Colegate <blaknite@thelanbox.com.au>
2017-02-22 15:59:49 +1030

    Ensure the controller/action combo are strings

--- lib/cancanright/controller_additions.rb
+++ lib/cancanright/controller_additions.rb
@@ -1,10 +1,10 @@
 module CanCanRight
   module ControllerAdditions
     def authorize_action!
-      controller = self.rights_from || params[:controller]
-      action = params[:action]
+      controller = (self.rights_from || params[:controller]).to_s
+      action = params[:action].to_s
 
-      return if can?(:access, controller) || can?(:access, "#{controller}##{action}")
+      return if can?(:access, controller) || can?(:access, controller + '#' + action)
 
       fail CanCan::AccessDenied, "You are not authorized to access this page."
     end
